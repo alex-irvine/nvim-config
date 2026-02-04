75bb3f10c375540f68aef0a9b698b02e23a606c7
Alex Irvine <alex.irvine@datascopesystems.com>
2026-02-04 14:50:15 +0000

    stop nvim changing working dir from language markers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.neo_tree_respect_gitignore = false
+
+-- Set root directory detection to prefer git root over language-specific project markers
+vim.g.root_spec = { ".git" }
