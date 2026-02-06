eb6f39a042e7a487545e478dbddd6fe95f9b0575
Alex Irvine <alex.irvine@datascopesystems.com>
2026-02-06 11:21:49 +0000

    disable swap files, auto linting, utility plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,8 @@
 
 -- Set root directory detection to prefer git root over language-specific project markers
 vim.g.root_spec = { ".git" }
+
+-- Disable swap files to avoid E325 warnings (safe in containers that are disposable)
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -9,5 +9,16 @@ return {
         typescriptreact = { "eslint_d" },
       },
     },
+    config = function(_, opts)
+      local lint = require("lint")
+      lint.linters_by_ft = opts.linters_by_ft
+
+      -- Auto-run linting on file events
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
   },
 }
